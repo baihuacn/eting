6c28cfe66611386b5e0fb88539c14b23ce9e30b1
Ren Junlin <junlin_ren@foxmail.com>
2017-11-25 15:04:06 +0800

    mixin err

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -1,17 +1,153 @@
 <template>
   <div class="music-list">
-    <div class="back">
+    <div class="back" @click="back">
       <i class="icon-back"></i>
     </div>
-    <h1 class="title"></h1>
-    <div class="bg-image">
-      <div class="filter"></div>
+    <h1 class="title">{{title}}</h1>
+    <div class="bg-image" :style="bgStyle" ref="bgImage">
+      <div class="play-wrapper">
+        <div @click="random" class="play" v-show="songs.length>0" ref="playBtn">
+          <i class="icon-play"></i>
+          <span class="text">随机播放全部</span>
+        </div>
+      </div>
+      <div class="filter" ref="filter"></div>
     </div>
+    <div class="bg-layer" ref="layer"></div>
+    <scroll
+      @scroll="scroll"
+      :probe-type="probeType"
+      :listen-scroll="listenScroll"
+      :data="songs"
+      class="list"
+      ref="list"
+    >
+      <div class="song-list-wrapper">
+        <song-list :rank="rank" @select="selectItem" :songs="songs"></song-list>
+      </div>
+      <div class="loading-container" v-show="!songs.length">
+        <loading></loading>
+      </div>
+    </scroll>
   </div>
 </template>
 
 <script>
+  import Scroll from 'base/scroll/scroll'
+  import SongList from 'base/song-list/song-list'
+  import {prefixStyle} from 'common/js/dom'
+  import Loading from 'base/loading/loading'
+  import {mapActions} from 'vuex'
+  import {playlistMixin} from 'common/js/mixin'
 
+  const transform = prefixStyle('transform')
+  const backdrop = prefixStyle('backdrop-filter')
+
+  const RESERVED_HEIGHT = 40
+
+  export default {
+    mixins: [playlistMixin],
+    props: {
+      bgImage: {
+        type: String,
+        default: ''
+      },
+      songs: {
+        type: Array,
+        default: []
+      },
+      title: {
+        type: String,
+        default: ''
+      },
+      rank:{
+        type:Boolean,
+        default:false
+      }
+    },
+    created(){
+      this.probeType = 3
+      this.listenScroll = true
+    },
+    data(){
+      return {
+        scrollY: 0
+      }
+    },
+    mounted(){
+      this.imageHeight = this.$refs.bgImage.clientHeight
+      this.minTranslateY = -this.imageHeight + RESERVED_HEIGHT
+      this.$refs.list.$el.style.top = `${this.imageHeight}px`
+    },
+    computed: {
+      bgStyle(){
+        return `background-image:url(${this.bgImage})`
+      }
+    },
+    methods: {
+      handlePlaylist(playlist){
+        const bottom = playlist.length > 0 ? '60px' : ''
+        this.$refs.list.$el.style.bottom = bottom
+        this.$refs.list.refresh()
+      },
+      // 选择音乐，展开播放界面
+      selectItem(item, index){
+        this.selectPlay({
+          list: this.songs,
+          index
+        })
+      },
+      scroll(pos){
+        this.scrollY = pos.y
+      },
+      back(){
+        this.$router.back()
+      },
+      random(){
+        this.randomPlay({
+          list: this.songs
+        })
+      },
+      ...mapActions([
+        'selectPlay',
+        'randomPlay'
+      ])
+    },
+    watch: {
+      scrollY(newY){
+        let translateY = Math.max(this.minTranslateY, newY)
+        let zIndex = 0
+        let scale = 1
+        let blur = 0
+        this.$refs.layer.style[transform] = `translate3d(0,${translateY}px,0)`
+        const percent = Math.abs(newY / this.imageHeight)
+        if (newY > 0) {
+          scale = 1 + percent
+          zIndex = 10
+        } else {
+          blur = Math.min(20 * percent, 20)
+        }
+        this.$refs.filter.style[backdrop] = `blur(${blur}px`
+        if (newY < this.minTranslateY) {
+          zIndex = 10
+          this.$refs.bgImage.style.paddingTop = 0
+          this.$refs.bgImage.style.height = `${RESERVED_HEIGHT}px`
+          this.$refs.playBtn.style.display = 'none'
+        } else {
+          this.$refs.bgImage.style.paddingTop = '70%'
+          this.$refs.bgImage.style.height = 0
+          this.$refs.playBtn.style.display = ''
+        }
+        this.$refs.bgImage.style.zIndex = zIndex
+        this.$refs.bgImage.style[transform] = `scale(${scale})`
+      }
+    },
+    components: {
+      Scroll,
+      SongList,
+      Loading
+    }
+  }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
@@ -27,7 +163,7 @@
     right: 0
     background: $color-background
     .back
-      position: absolute
+      position absolute
       top: 0
       left: 6px
       z-index: 50
